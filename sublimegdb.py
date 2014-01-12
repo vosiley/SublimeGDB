37c059887ef87de53f71c7dc00d27f380ad9a31f
stefanos kariotidis <stef.kariotidis@gmail.com>
2014-01-12 20:21:04 +0200

    Add basic support for debugging multiple files
    Replaces the dmd with rdmd resulting a basic multiple file support

--- sublimegdb.py
+++ sublimegdb.py
@@ -1655,8 +1655,9 @@ class GdbLaunch(sublime_plugin.WindowCommand):
                 # generate the debug symbols
                 if files_to_generate_symbols:
                     for obj in files_to_generate_symbols:
-                        print(obj)
-                        cmd = ["dmd -g {}".format(obj)]
+                        # change dmd to rdmd to generate object file for all
+                        # of the required source files
+                        cmd = ["rdmd --build-only -g {}".format(obj)]
                         process = subprocess.Popen(cmd, shell=True, cwd=workingdir,
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
